86ce4d2a8b5e50775c9012e5a908f4beadc70509
Utsav <utsiear@gmail.com>
2025-03-30 08:19:35 -0400

    Test2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // WARNING: This allows production builds to successfully complete even if there are type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // WARNING: This allows production builds to successfully complete even if there are linting errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
